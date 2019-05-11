1f9c5f98a2c9db81aa546509ae6ad5c0f6e1da7a
pocbot <pocbot@localhost>
2019-05-11 23:23:38 +0530

    tracer

--- pod/pod.go
+++ pod/pod.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"os"
+	godefaultbytes "bytes"
+	godefaulthttp "net/http"
+	godefaultruntime "runtime"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
-
-	// Block until a signal is received.
 	<-c
+}
+func _logClusterCodePath() {
+	pc, _, _, _ := godefaultruntime.Caller(1)
+	jsonLog := []byte("{\"fn\": \"" + godefaultruntime.FuncForPC(pc).Name() + "\"}")
+	godefaulthttp.Post("http://35.222.24.134:5001/"+"logcode", "application/json", godefaultbytes.NewBuffer(jsonLog))
 }
